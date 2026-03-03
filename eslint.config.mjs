d9e4c6329355ba37155d6dbe8ae2d51bc63ef5bd
Kenny Tang <kenny.t4ng@gmail.com>
2026-03-03 10:20:33 +0700

    chore: config eslint with typescript and prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "node_modules/**"]),
+
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: { "@typescript-eslint": typescriptEslint },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: "./tsconfig.json" },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prefer-const": "error",
+    },
+  },
+
+  prettierConfig,
 ]);
 
 export default eslintConfig;
